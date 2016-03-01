1512e10c4ea47b924397de1dd753e5036f7663bc
liuhaixia888 <haixialiu@haixialiudeiMac.local>
2016-03-01 18:49:41 +0800

    更新组件；增加对不需要进行转换的样式单独处理

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var config = {};
 var extend = require('aimee-extend');
+var lib = require('linco.lab').lib;
+var map = {};
 
 var cvert = {
 
@@ -10,7 +12,7 @@ var cvert = {
             [s2 + val, 'rem' + s5].join('');
     },
 
-	// AM => PX
+    // AM => PX
     am2px: function(){
         var arr = [].slice.call(arguments, 0);
         var s1 = arr.shift();
@@ -18,7 +20,7 @@ var cvert = {
             s1 + arr.join('') : arr.join('');
     },
 
-	// REM => PX
+    // REM => PX
     rem2px: function(s1, s2, s5, val){
         return s1 ?
             [s1 + s2 + val, 'px' + s5].join(''):
@@ -33,25 +35,25 @@ var cvert = {
         }
 
         return config.rounding === 'none' ?
-        	// 如果为true，不进行四舍五入，默认浏览器去处理
+            // 如果为true，不进行四舍五入，默认浏览器去处理
             (num/config.constant *100)/100 :
             // 如果为false，调用Math方法
             Math[config.rounding](num/config.constant *100)/100;
     },
 
-	// REM => PX四舍五入的处理
+    // REM => PX四舍五入的处理
     roundingREM: function(num){
         // 非法值默认处理为none
         if(config.rounding !== 'none' && !Math[config.rounding]){
             config.rounding = 'none'
         }
 
-        return config.rounding === 'none' ?        	
+        return config.rounding === 'none' ?         
             parseInt((num*config.constant *100)/100) :
             (Math[config.rounding](num*config.constant *100)/100).toString().indexOf('.0') > -1 ? 
-            	// 如果结果后边有小数'.0'，直接取整，否则调用Math方法
-            	parseInt(Math[config.rounding](num*config.constant *100)/100):
-            	Math[config.rounding](num*config.constant *100)/100;
+                // 如果结果后边有小数'.0'，直接取整，否则调用Math方法
+                parseInt(Math[config.rounding](num*config.constant *100)/100):
+                Math[config.rounding](num*config.constant *100)/100;
     }
 
 }
@@ -63,15 +65,15 @@ module.exports = function(ret, conf, settings, opt) {
     var ct = {
 
          /**
-		 * PX/REM/AM相互转换
-		 * @param  {String}   content    整个样式文件内容
-		 * @param  {number}   constant   转换的常量值默认100, 1rem=100px, 可以是整数/小数
+         * PX/REM/AM相互转换
+         * @param  {String}   content    整个样式文件内容
+         * @param  {number}   constant   转换的常量值默认100, 1rem=100px, 可以是整数/小数
          * @param  {String}   convertTo  默认px, 将rem转换为px
          * @param  {String}   rounding   默认round四舍五入; ceil向下取整; floor向上取整; none不做任何处理
          * @param  {Boolean}  am2px      默认false, 为true时样式表中所有的am转换为px
          * @param  {Boolean}  border     默认false, border：true且border1px：false, 除了border 1px, 其他的都将px转化为rem
          * @param  {Boolean}  border1px  默认false, border：true且border1px：true, 所有的px转换为rem
-		 */
+         */
 
         //PX => REM
         _px2rem: function(content){
@@ -79,7 +81,7 @@ module.exports = function(ret, conf, settings, opt) {
 
                 // PX => REM
                 if(/px/.test($4)){
-                	// 检查border如果为true, 除border为1px, 返回其他PX=>REM的处理结果
+                    // 检查border如果为true, 除border为1px, 返回其他PX=>REM的处理结果
                     if(config.border){
                         // 检查border1px如果为true，返回所有PX=>REM的处理结果
                         return config.border1px ?
@@ -89,7 +91,7 @@ module.exports = function(ret, conf, settings, opt) {
                     }
                     // 检查border如果为false，除border，返回其他PX=>REM的处理结果
                     else{
-                    	// 检查匹配到的$1为undefined，返回除border之外的其他结果，如果$1不为undefined不做任何处理返回原字符串
+                        // 检查匹配到的$1为undefined，返回除border之外的其他结果，如果$1不为undefined不做任何处理返回原字符串
                         return $1 ? s :
                             cvert.px2rem($1, $2, $5, cvert.roundingPX($3));
                     }
@@ -97,7 +99,7 @@ module.exports = function(ret, conf, settings, opt) {
 
                 // AM => PX
                 return config.am2px && /am/.test($4) ?
-                	// 检查am2px如果为true，直接返回AM=>PX的处理结果，否则返回原字符串
+                    // 检查am2px如果为true，直接返回AM=>PX的处理结果，否则返回原字符串
                     cvert.am2px($1, $2, $3, $4.replace('am', 'px'), $5) : s;
 
             });
@@ -108,34 +110,57 @@ module.exports = function(ret, conf, settings, opt) {
         _rem2px: function(content){
             return content.replace(regx,function(s, $1, $2, $3, $4, $5){
 
-            	// REM => PX
-				if(/rem/.test($4)){
-					// 检查border如果为true, 除border为1px, 返回其他REM=>PX的处理结果					
-					if(config.border){
+                // REM => PX
+                if(/rem/.test($4)){
+                    // 检查border如果为true, 除border为1px, 返回其他REM=>PX的处理结果                   
+                    if(config.border){
                         return config.border1px ?
                             cvert.rem2px($1, $2, $5, cvert.roundingREM($3)):
                             // 如果为false，检查 $3 === 1返回原字符串，否则返回REM=>PX的处理结果
                             Number($3) === 1 ? s : cvert.rem2px($1, $2, $5, cvert.roundingREM($3));
 
-					}
-					// 检查border如果为false, 除border, 返回其他REM=>PX的处理结果
-					else{
-						// 检查匹配到的$1为undefined，返回除border之外的其他结果，如果$1不为undefined不做任何处理返回原字符串
+                    }
+                    // 检查border如果为false, 除border, 返回其他REM=>PX的处理结果
+                    else{
+                        // 检查匹配到的$1为undefined，返回除border之外的其他结果，如果$1不为undefined不做任何处理返回原字符串
                         return $1 ? s :
                             cvert.rem2px($1, $2, $5, cvert.roundingREM($3));
-					}
-				}
+                    }
+                }
 
                 // AM => PX
                 return config.am2px && /am/.test($4) ?
-                	// 检查am2px如果为true，直接返回AM=>PX的处理结果，否则返回原字符串
+                    // 检查am2px如果为true，直接返回AM=>PX的处理结果，否则返回原字符串
                     cvert.am2px($1, $2, $3, $4.replace('am', 'px'), $5) : s;
 
             });
 
+        },
+
+        // AM => PX
+        _am2px: function(content){
+            return content.replace(regx, function(s, $1, $2, $3, $4, $5){
+                // AM => PX
+                return /am/.test($4) ?
+                    // 检查am2px如果为true，直接返回AM=>PX的处理结果，否则返回原字符串
+                    cvert.am2px($1, $2, $3, $4.replace('am', 'px'), $5) : s;
+            });           
         }
 
     };
+
+    //如果没有配置, 直接将页面中的am单位替换为px
+    if(lib.isEmptyObject(settings)){
+        
+        for(key in ret.pkg){
+            if(key.indexOf('.css') > -1){
+                if(!map[key]){
+                    ret.pkg[key]._content = ct._am2px(ret.pkg[key]._content);
+                    map[key] = true;
+                }
+            }
+        }
+    }      
 
     //如果有配置, 找到打包后的所有css文件, 根据配置参数进行单位转换并重新写入文件
     fis.util.map(settings, function(file, opt){
@@ -146,17 +171,20 @@ module.exports = function(ret, conf, settings, opt) {
             am2px: false,
             border: false,
             border1px: false
-        }
+        }      
 
         //参数合并
         config = extend({}, defaults, opt);
 
         for(key in ret.pkg){
             if(key.indexOf(file) > -1){
-            	
-            	config.convertTo == 'rem' ?
-            		ret.pkg[key]._content = ct._px2rem(ret.pkg[key]._content):
-            		ret.pkg[key]._content = ct._rem2px(ret.pkg[key]._content);
+                if(!map[key]){
+                    config.convertTo == 'rem' ?
+                        ret.pkg[key]._content = ct._px2rem(ret.pkg[key]._content):
+                        ret.pkg[key]._content = ct._rem2px(ret.pkg[key]._content);
+                    map[key] = true;
+                }               
+
             }
         }
 
